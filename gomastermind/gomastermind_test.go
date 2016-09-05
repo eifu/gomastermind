84c499e8085ccdb17a94e877e9fa10424d8dc8e4
eifu <etomita18@gmail.com>
2016-09-04 23:06:01 -0400

    add test case for b1w1, b0w3, b2w0 patterns

--- gomastermind/gomastermind_test.go
+++ gomastermind/gomastermind_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestHash(t *testing.T) {
 
-	test1 := []byte{'R', 'W', 'Y', 'G'}
+	test1 := []byte{R, W, Y, G}
 
 	if !bytes.Equal(test1, Dehash(Hash(test1))) {
 		t.Error("Expected [82 87 89 71], got", Dehash(Hash(test1)))
@@ -23,11 +23,11 @@ func TestHash(t *testing.T) {
 }
 
 func TestElimColor(t *testing.T) {
-	var colors []byte = []byte{'W', 'Y', 'G', 'U', 'K'}
+	var colors []byte = []byte{W, Y, G, U, K}
 
-	var c1 []byte = []byte{'R', 'W', 'Y', 'G', 'U', 'K'}
+	var c1 []byte = []byte{R, W, Y, G, U, K}
 
-	c2 := elimColor('R', c1)
+	c2 := elimColor(R, c1)
 
 	if !bytes.Equal(c2, colors) {
 		t.Error("Expected", colors, ", got", c2)
@@ -42,6 +42,7 @@ func TestPermute(t *testing.T) {
 }
 
 func TestCase1(t *testing.T) {
+	fmt.Println("case1:")
 	expected := []byte{R, U, Y, Y}
 	pool := make([]int, 6*6*6*6)
 	for i := 0; i < 6*6*6*6; i++ {
@@ -78,7 +79,9 @@ func TestCase1(t *testing.T) {
 }
 
 func TestCase2(t *testing.T) {
-
+	fmt.Println("case2:")
+	expected1 := []byte{G, K, G, Y}
+	expected2 := []byte{G, W, G, U}
 	pool := make([]int, 6*6*6*6)
 	for i := 0; i < 6*6*6*6; i++ {
 		pool[i] = 1
@@ -110,6 +113,43 @@ func TestCase2(t *testing.T) {
 	for i := 0; i < 6*6*6*6; i++ {
 		if pool[i] != 0 {
 			fmt.Println(string(Dehash(i)))
+			if Hash(expected1) != i && Hash(expected2) != i {
+				t.Error("Expected", expected1, "or", expected2, ", got", string(Dehash(i)))
+			}
+		}
+	}
+}
+
+func TestCase3(t *testing.T) {
+	fmt.Println("case3:")
+	pool := make([]int, 6*6*6*6)
+	for i := 0; i < 6*6*6*6; i++ {
+		pool[i] = 1
+	}
+
+	var guess_l []byte
+	var score_l []int
+
+	guess_l = []byte{W, G, U, R}
+	score_l = []int{1, 1}
+	pool = Finder(guess_l, score_l, pool)
+
+	guess_l = []byte{K, Y, R, U}
+	score_l = []int{1, 1}
+	pool = Finder(guess_l, score_l, pool)
+
+	guess_l = []byte{R, R, G, Y}
+	score_l = []int{0, 3}
+	pool = Finder(guess_l, score_l, pool)
+
+	guess_l = []byte{Y, R, R, W}
+	score_l = []int{2, 0}
+	pool = Finder(guess_l, score_l, pool)
+
+	for i := 0; i < 6*6*6*6; i++ {
+		if pool[i] != 0 {
+			fmt.Println(string(Dehash(i)))
+
 		}
 	}
 }
