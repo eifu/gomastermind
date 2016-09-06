787abd84c929434898a3d3010bf53abe3d640bda
eifu <etomita18@gmail.com>
2016-09-06 12:46:28 -0400

    change error statement

--- main.go
+++ main.go
@@ -19,25 +19,25 @@ func main() {
 	fmt.Println("enter the num of sequence")
 	sNbyte, err := reader.ReadBytes('\n')
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "reader:1 %v\n", sNbyte, err)
+		fmt.Fprintf(os.Stderr, "ReadBytes error: %v\n", sNbyte, err)
 		os.Exit(1)
 	}
 
 	fmt.Println("enter the num of color")
 	cNbyte, err := reader.ReadBytes('\n')
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "reader:3 %v\n", cNbyte, err)
+		fmt.Fprintf(os.Stderr, "ReadBytes error: %v\n", cNbyte, err)
 		os.Exit(1)
 	}
 
 	sN, err = strconv.Atoi(string(sNbyte[0]))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "reader:5 %v\n", sNbyte, err)
+		fmt.Fprintf(os.Stderr, "Strconv Atoi: %v\n", sNbyte, err)
 		os.Exit(1)
 	}
 	cN, err = strconv.Atoi(string(cNbyte[0]))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "reader:6 %v\n", cNbyte, err)
+		fmt.Fprintf(os.Stderr, "Strconv Atoi: %v\n", cNbyte, err)
 		os.Exit(1)
 	}
 
@@ -53,7 +53,7 @@ func main() {
 		fmt.Println("enter color: red(R), white(W), yellow(Y), green(G), blue(Bu), and black(Bk).")
 		guess, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "reader: %v\n", guess, err)
+			fmt.Fprintf(os.Stderr, "ReadString error: %v\n", guess, err)
 			os.Exit(1)
 		}
 		guess_l = gomastermind.SplitGuess(guess)
@@ -61,7 +61,7 @@ func main() {
 		fmt.Println("enter score: x for right color, right position, o for right color but in the wrong position.")
 		score, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "reader: %v\n", score, err)
+			fmt.Fprintf(os.Stderr, "ReadString error: %v\n", score, err)
 			os.Exit(1)
 		}
 
@@ -77,7 +77,7 @@ func main() {
 			}
 		}
 
-		fmt.Println(count, "cases found")
+		fmt.Printf("%d cases found\n\n", count)
 
 	}
 }
